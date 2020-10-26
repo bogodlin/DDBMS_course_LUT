f5571939da4a5442898f551532cbf8b756d24e49
godlin <bogodline@gmail.com>
2020-10-26 02:52:15 +0200

    Removed TODO's

--- eye_for_eye/__init__.py
+++ eye_for_eye/__init__.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-#TODO deal with SQLALCHEMY_DATABASE_URI env variable
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/app"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
