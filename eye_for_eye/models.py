d17cd7c7be7fe14354de68eecddb6ea160fa2b7d
bogodline <37313677+bogodline@users.noreply.github.com>
2020-11-12 23:25:36 +0200

    Update models.py
    Added citizen object to models.py

--- eye_for_eye/models.py
+++ eye_for_eye/models.py
@@ -2,6 +2,23 @@ rom eye_for_eye import db, app
 from sqlalchemy.dialects.postgresql import ARRAY
 from datetime import datetime
 
+class Citizen(db.Model):
+
+    __tablename__ = 'citizen'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    surname = db.Column(db.String, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(120), nullable=True)
+    phone_number = db.Column(db.String(15), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='citizen_default.png')
+    cases = db.relationship('Case', backref='case_citizen', lazy=True)
+    country = db.Column(db.Integer, db.ForeignKey('country.id'))
+
+    def __repr__(self):
+        return f"Citizen('{self.name}', '{self.surname}', '{self.email}', '{self.phone_number}')"
+
 class Case(db.Model):
 
     tablename = 'case'
