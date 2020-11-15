e6488cd3c98aa1253e343394d9c5a0b0cb595f56
godlin <bogodline@gmail.com>
2020-11-16 00:55:12 +0200

    Added surname authentication check in e4e regular

--- eye_for_eye/routes.py
+++ eye_for_eye/routes.py
@@ -2,7 +2,7 @@ import os
 from eye_for_eye import app
 from flask import render_template, flash, redirect, url_for, session, request
 from eye_for_eye.forms import *
-from eye_for_eye.models import Case
+from eye_for_eye.models import Case, Citizen
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -12,7 +12,11 @@ def home():
     if form.validate_on_submit():
         case = Case.query.filter_by(code=form.case_code.data).first()
         if case:
-            return redirect(url_for('view_case', case_code=case.code))
+            citizen = Citizen.query.filter_by(id=case.citizen).first()
+            if citizen.surname == form.surname.data:
+                return redirect(url_for('view_case', case_code=case.code))
+            else:
+                flash(f'Please, check the surname field once again', 'danger')
         else:
             flash(f'No case with {form.case_code.data} is found', 'danger')
     return render_template('home.html', form = form)
